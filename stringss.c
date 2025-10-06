92223be2f0bd8c6546fcfa94505a7026f18f7ac0
Aneesh Datta <aneeshdatta.jois@gmail.com>
2025-10-06 23:16:38 +0530

    jsdkfjhsdf

--- stringss.c
+++ stringss.c
@@ -3,3 +3,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 //#include <string.h>
+
+int main(){
+    char str[] = "slim shady";
+    char str1[20];
+    printf("enter a string\n");
+    scanf("%s",&str1); 
+    printf("the entered string is %s\n",str1);
+    return 0;
+}
